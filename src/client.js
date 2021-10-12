ed3ca3090e930dbaf71750c5ece03c2eb982e8d5
IbrahimG88 <goharyibrahim@gmail.com>
2021-10-12 16:55:29 +0200

    Add schema  and react component item project

--- src/client.js
+++ src/client.js
@@ -3,6 +3,6 @@ import sanityClient from "@sanity/client";
 export default sanityClient({
   projectId: "669bf58y",
   dataset: "production",
-  apiVersion: "2021-10-11",
+  apiVersion: "2021-03-25",
   useCdn: "false",
 });
